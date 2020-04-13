1fe1085d2cb89fc48731274f8bf8fd1135cf6d63
Naoto Miyakawa <naoto@mba2019.kkbnj1.kt.home.ne.jp>
2020-04-13 22:20:54 +0900

    ひより

--- twitter/utils/ClassifyTweet.py
+++ twitter/utils/ClassifyTweet.py
@@ -1,7 +1,7 @@
 def ClassifyTweet(text):
     text = text.strip("@mHiyori0324").strip(" ").strip("　").strip("\n")
     print(text)
-    if text == "天気を教えて。":
+    if text == "天気を教えて、ひよりちゃん":
         return "weather"
     else:
         return "markov"
