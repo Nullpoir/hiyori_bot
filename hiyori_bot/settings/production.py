fc532a12f15b791e2024c9fb61c76233c0e6a04b
Naoto Miyakawa <naoto@mba2019.kkbnj1.kt.home.ne.jp>
2020-04-14 23:00:07 +0900

    追加コミット

--- hiyori_bot/settings/production.py
+++ hiyori_bot/settings/production.py
@@ -3,21 +3,6 @@ from celery.schedules import crontab
 
 DEBUG = False
 
-#twitter
-
-TWITTER_CONSUMER_KEY = os.environ.get('TWITTER_CONSUMER_KEY')
-TWITTER_CONSUMER_SECRET = os.environ.get('TWITTER_CONSUMER_SECRET')
-TWITTER_TOKEN = os.environ.get('TWITTER_TOKEN')
-TWITTER_TOKEN_SECRET = os.environ.get('TWITTER_TOKEN_SECRET')
-MY_ID = os.environ.get('MY_ID')
-
-DB_NAME = os.environ.get('DB_NAME')
-DB_USER = os.environ.get('DB_USER')
-DB_PASSWORD = os.environ.get('DB_PASSWORD')
-
-OPENWETHERMAP_API_KEY = os.environ.get('OPENWETHERMAP_API_KEY')
-OPENWETHERMAP_URL = os.environ.get('OPENWETHERMAP_URL')
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
