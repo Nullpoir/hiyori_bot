5ead4506478b701f87a31db4c7f6541e311b3ba7
Naoto Miyakawa <naoto@mba2019.local>
2020-04-15 01:41:30 +0900

    修正

--- hiyori_bot/settings/production.py
+++ hiyori_bot/settings/production.py
@@ -20,3 +20,4 @@ CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TIMEZONE = 'Asia/Tokyo'
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers.DatabaseScheduler'
