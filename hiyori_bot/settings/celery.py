907bf99cfce239e79d3a25ae063d11fce606be85
Naoto Miyakawa <naoto@mba2019.local>
2020-04-15 14:30:43 +0900

    環境設定

--- hiyori_bot/settings/celery.py
+++ hiyori_bot/settings/celery.py
@@ -3,10 +3,7 @@ from celery import Celery
 import os
 
 # set the default Django settings module for the 'celery' program.
-if os.environ.get('Production') != None:
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hiyori_bot.settings.production")
-else:
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hiyori_bot.settings.local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hiyori_bot.settings.production")
 
 app = Celery('hiyori_bot',broker='redis://127.0.0.1:6379/1')
 app.config_from_object('django.conf:settings', namespace='CELERY')
