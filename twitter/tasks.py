71af4fdf864f3def9f6c9a8dcfca9fe788809196
Naoto Miyakawa <naoto@mba2019.local>
2020-05-28 01:24:39 +0900

    テスト

--- twitter/tasks.py
+++ twitter/tasks.py
@@ -57,7 +57,7 @@ def get_maiko_tweets():
     #まいこ先生Tweet取得
     for status in api.user_timeline(id='uma401'):
         # RTとリプライを除外
-        if not status.retweeted or in_reply_to_status_id != None:
+        if not status.retweeted or status.in_reply_to_status_id != None:
             print("除外tweet")
             print(status.text)
             continue
