475caba1b8759ff2ce4834570dad74f92fabdf56
Naoto Miyakawa <naoto@mba2019.local>
2020-05-29 23:13:15 +0900

    1分に一度

--- twitter/tasks.py
+++ twitter/tasks.py
@@ -63,7 +63,7 @@ def get_maiko_tweets():
 
     #クエリ生成
     id='uma401'
-    since = datetime.datetime.utcnow() - datetime.timedelta(minutes=2)
+    since = datetime.datetime.utcnow() - datetime.timedelta(minutes=1)
     query = "from:" + id + " since:" + since.strftime("%Y-%m-%d_%H:%M:%S_UTC")
 
     #Discord WebHook接続
