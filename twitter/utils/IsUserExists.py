a54f40dc25dcabd10f41974c007643a7e02b90a6
Naoto Miyakawa <naoto@mba2019.kkbnj1.kt.home.ne.jp>
2020-07-04 17:48:33 +0900

    修正

--- twitter/utils/IsUserExists.py
+++ twitter/utils/IsUserExists.py
@@ -1,8 +1,10 @@
 from core.models import User
 
 def is_twitter_user_exists(twitter_id):
+    if !(type(twitter_id) is str)
+        twitter_id = str(twitter_id)
     try:
-        user = User.objects.get(twitter_id=str(id))
+        user = User.objects.get(twitter_id=twitter_id)
     except:
         user = None
     if user == None:
